3d37dd27004e48efbef5d2f3970fa6e062dd6554
Serg <7772223psp@gmail.com>
2023-04-25 10:27:20 +0300

    Мой третий коммит. Добавлена папка ресурсы. Начато заполнение проект.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-showMessege = () => {
-    console.log('Привет Мир!')
-};
-showMessege ();
+function setButtonBehavior(selector, event, callback) {
+    const elem = document.querySelector(selector);
+    elem.addEventListener(event, callback)
+}
+
+function setAlert() {
+    alert('Обработка клика!')
+}
+
+setButtonBehavior('.mainPage', 'click', setAlert);
+setButtonBehavior('.projects', 'click', setAlert);
+setButtonBehavior('.contacts', 'click', setAlert);
+
